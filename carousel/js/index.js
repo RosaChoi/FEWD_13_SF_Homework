31730f99be2de39748ab391095286ec7f6ec3f26
waselrosa <rosa@waselrosa.com>
2014-02-13 19:25:01 -0800

    Attaching Event Listener to buttons

---
+++ carousel/js/index.js
@@ -0,0 +1,13 @@
+	$('#btn-skip').on('click', function(e) {
+		e.preventDefault();
+		console.log('skip clicked');
+
+	});
+
+	$('#btn-back').on('click', function(e) {
+		e.preventDefault();
+		console.log('back clicked');
+	});
+
+
+
